f4f454d8acf4d2adfaee95c1898dfa8d7516ba68
MathewBradshaw23 <mat_2395@hotmail.com>
2019-08-16 12:17:32 +1000

    Added actor and movie-actor tables to migration

--- database/migrations/2019_08_15_232948_create_movies_table.php
+++ database/migrations/2019_08_15_232948_create_movies_table.php
@@ -16,6 +16,21 @@ class CreateMoviesTable extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
+            $table->string('Image');
+            $table->string('Description');
+            $table->float('Rating');
+            $table->timestamps();
+        });
+        Schema::create('actor', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fName');
+            $table->string('lName');
+            $table->string('DoB');
+            $table->timestamps();
+        });
+        Schema::create('movie-actor', function (Blueprint $table) {
+            $table->integer('movie_ID');
+            $table->integer('actor_ID');
             $table->timestamps();
         });
     }
